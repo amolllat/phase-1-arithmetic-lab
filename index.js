b1d325a00dbc5cc9759ecb3e2274ab20f3ca095c
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-02-18 22:28:27 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// My 1st point code here
+let num1 = 31 ;
+let num2 = 2 ;
+let multiply = (num1*num2) ;
+console.log(multiply);
+// My 2ed point code here
+ let random = Math.floor(Math.random()*20) +1 ;
+ console.log(random);
+// My 3ed point code here
+let num3 = 14 ;
+let num4 = 10 ;
+let mod = num3 % num4 ;
+console.log(mod);
+// My 4th point code here
+let max =Math.max(10,5,20,15);
+console.log(max);
 
-// Write your code here
